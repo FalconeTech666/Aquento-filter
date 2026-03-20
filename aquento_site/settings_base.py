aef8d8633ed001a9cd5f0754df406704b4a34227
Ilya <sokolovmslu@gmail.com>
2026-03-20 18:41:45 +0300

    6

--- aquento_site/settings_base.py
+++ aquento_site/settings_base.py
@@ -62,9 +62,9 @@ TIME_ZONE = "America/Toronto"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
